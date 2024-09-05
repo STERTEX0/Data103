a50708e70bd17841cc20c0c38f9629cf7dc2ef37
STERTEX0 <patipan-b@rmutp.ac.th>
2024-09-05 14:42:44 +0700

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 // src/components/Home.js
 
-import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import ListWrapper from './ListWrapper';
-import ImageTextSection from './ImageTextSection'; 
-import GridSection from './GridSection'; 
-import LocationGrid from './LocationGrid'; 
-import './Home.css'; // ไฟล์ CSS สำหรับหน้า Home
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import ListWrapper from "./ListWrapper";
+import ImageTextSection from "./ImageTextSection";
+import GridSection from "./GridSection";
+import LocationGrid from "./LocationGrid";
+import "./Home.css"; // ไฟล์ CSS สำหรับหน้า Home
 
 const Home = () => {
   return (
@@ -20,20 +20,20 @@ const Home = () => {
         <LocationGrid />
         <div className="video-section">
           <div className="video-container">
-          <iframe 
-  width="560" 
-  height="315" 
-  src="https://www.youtube.com/embed/sSec6Oxi5nA?si=K61uVjBxc6rxkMFJ" 
-  title="YouTube video player" 
-  frameBorder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-  referrerPolicy="strict-origin-when-cross-origin" 
-  allowFullScreen
-></iframe>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/SO_d7yD_FtY?si=TJ-_9W7w6GY_F2gR"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
           </div>
           <div className="video-container">
-            <iframe 
-              src="https://www.youtube.com/embed/VIDEO_ID2" 
+            <iframe
+              src="https://www.youtube.com/embed/sEZPHs9dVUM?si=bCCRpipO1vOFg1b0"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
